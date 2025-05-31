c39d84a199afd6c63edb2db89d6b34d6eef16e61
Vikas Kumar <22bcs13775@gmail.com>
2025-05-31 17:27:29 +0530

    Create outputs.tf

---
+++ partA/outputs.tf
@@ -0,0 +1,11 @@
+output "linux_vm_private_ip" {
+  value = azurerm_network_interface.linux_nic.ip_configuration[0].private_ip_address
+}
+
+output "windows_vm_private_ip" {
+  value = azurerm_network_interface.windows_nic.ip_configuration[0].private_ip_address
+}
+
+output "sql_vm_private_ip" {
+  value = azurerm_network_interface.sql_nic.ip_configuration[0].private_ip_address
+}
