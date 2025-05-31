b3d215ba00a2e5ea3597f6852fbb726db1ec0bb2
Vikas Kumar <22bcs13775@gmail.com>
2025-05-31 17:37:15 +0530

    Create variables.tf

---
+++ partB/variables.tf
@@ -0,0 +1,13 @@
+variable "resource_group_name" {
+  description = "Resource Group Name"
+  type        = string
+  default     = "rg-partB"
+}
+
+variable "location" {
+  description = "Azure Location"
+  type        = string
+  default     = "eastus"
+}
+
+variable "admin
